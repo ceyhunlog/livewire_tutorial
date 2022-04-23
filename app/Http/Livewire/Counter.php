a602bd4aefad590f18185f2e606e47e957516b06
ceyhunlog <ceyhunbcelik.log@gmail.com>
2022-04-23 12:04:30 +0300

    count increase and decrease

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -6,8 +6,20 @@ use Livewire\Component;
 
 class Counter extends Component
 {
+    public int $count = 10;
+
     public function render()
     {
         return view('livewire.counter');
+    }
+
+    public function increase()
+    {
+        $this->count++;
+    }
+
+    public function decrease()
+    {
+        $this->count--;
     }
 }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,9 @@
 <div>
-    <h1>Counter</h1>
+    <h1>Counter: {{ $count }}</h1>
+    <div>
+        <div class="col-12">
+            <button wire:click="increase" class="btn btn-success">+</button>
+            <button wire:click="decrease" class="btn btn-danger">-</button>
+        </div>
+    </div>
 </div>
